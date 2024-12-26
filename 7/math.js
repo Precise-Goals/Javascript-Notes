da3e46d28ccf1a5e4bae2610090a3925d4186fb7
Precise Goals <elitesarthakamr@gmail.com>
2024-12-26 11:26:18 +0530

    Maths Related javascript

---
+++ 7/math.js
@@ -0,0 +1,26 @@
+// Math.round() Rounding off
+// Math.floor() gives GIF Function rounds down
+// Math.ceil() rounds up
+// Math.Pi pie value
+console.log(Math.PI);
+// Math.abs() absolute value of number shows how far is the num from 0
+// Math.trunc  gives the integer part of any decimal pointed number
+// Math.pow gives the exponential power
+// Math.sqrt gives the square root
+// Math.log() give logarithmic expression
+// Math.random gives random number between 0 (inclusive) to 1 (exclusive)
+
+console.log(Math.round(-5.7));
+console.log(Math.floor(-5.7));
+console.log(Math.ceil(-5.7));
+console.log(Math.PI);
+console.log(Math.abs(789));
+console.log(Math.trunc(Math.PI));
+console.log(Math.pow(Math.PI, 3));
+console.log(Math.PI ** 3);
+console.log(Math.sqrt(Math.PI));
+console.log(Math.pow(Math.PI, 0.5));
+console.log(Math.log(Math.PI));
+console.log(Math.log(1));
+console.log(Math.round(Math.random() * 100));
+console.log((Math.random() * 100).toFixed(3));
